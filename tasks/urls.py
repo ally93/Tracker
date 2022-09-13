f29ff6a734115d819782bdbb553ab26f258dafa4
Ally <allygoose93@gmail.com>
2022-09-13 12:22:16 -0700

    Feature 17 complete

--- tasks/urls.py
+++ tasks/urls.py
@@ -2,11 +2,13 @@ from django.urls import path
 
 from tasks.views import (
     TaskCreateView,
-    TaskListView
+    TaskListView,
+    TaskUpdateView
 )
 
 urlpatterns = [
     path("create/", TaskCreateView.as_view(), name="create_task"),
-    path("mine/", TaskListView.as_view(), name="show_my_tasks")
+    path("mine/", TaskListView.as_view(), name="show_my_tasks"),
+    path("<int:pk>/complete/", TaskUpdateView.as_view(), name="complete_task")
 
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.list import ListView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from tasks.models import Task
 from django.urls import reverse_lazy
 
@@ -14,9 +14,19 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def get_success_url(self):
         return reverse_lazy("show_project", args=[self.object.project.id])
 
+
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
     template_name = "tasks/list.html"
 
     def get_queryset(self):
         return Task.objects.filter(assignee=self.request.user)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    template_name = "tasks/list.html"
+    fields = ["is_completed"]
+
+    def get_success_url(self):
+        return reverse_lazy("show_my_tasks")
